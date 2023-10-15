31b21edf2a1479f64fe61e7e8fd027a8f1067976
Lucifer <patilneeraj2003@gmail.com>
2023-10-15 23:33:11 +0530

    added server config

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,31 @@ import RegisterPage from './pages/RegisterPage'
 
 function App() {
 
+  const express = require('express');
+  const app = express();
+
+  // Configure CORS to allow requests from specific origins
+  const allowedOrigins = ['https://chat-app-neeraaaj.vercel.app'];
+
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    // Add other CORS headers as needed
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
+  // Your other route handling code goes here
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+
   return (
     <Router>
       <AuthProvider>
